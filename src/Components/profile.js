352ffb0a77b54d57bdb1464dc684aeb2ee073823
github username <github email address>
2019-07-07 20:43:03 -0400

    moved state to profile component

--- src/Components/profile.js
+++ src/Components/profile.js
@@ -8,11 +8,145 @@ import SpendStats from './spendStats'
 
 class Profile extends React.Component {
 
-  // constructor(props) {
-  // super(props);
-  // // this.state = {
-  // //   }
-  // }
+
+  state={
+    purchases:[],
+    date:"",
+    name:"",
+    category:"",
+    placeOfPurchase:"",
+    outOfPocket:"",
+    actualPaid:"",
+    paymentMethod:"",
+    selected: "",
+    clickedRowId: "",
+    spent: 0.0
+  }
+
+
+    componentDidMount() {
+      fetch(`http://localhost:3000/${localStorage.user_id}/purchases`)
+      .then(resp => resp.json())
+      .then(purchaseArr => {
+      let spend = []
+      const reducer = (accumulator, currentValue) => accumulator + currentValue;
+      let spendCalc = purchaseArr.purchase.forEach( purchase => spend.push(parseInt(purchase.actual_paid) ))
+      let total = parseFloat(spend.reduce(reducer)).toFixed(2)
+      console.log(total);
+       this.setState({
+       purchases: purchaseArr.purchase,
+       spent: total
+       })
+     })
+   }
+
+   handleChange = (e) => {
+     this.setState({
+       [e.target.name]: e.target.value
+     })
+   }
+
+   handlePaymentChange = (e) => {
+     this.setState({
+        paymentMethod: e.currentTarget.innerText
+     })
+   }
+
+   handleCategoryChange = (e) => {
+     this.setState({
+        category: e.currentTarget.innerText
+     })
+   }
+
+   // handleSelectChange=(e,{value})=>this.setState({paymentMethod:value})
+
+   handleSubmit = (e) => {
+     e.preventDefault()
+
+     let purObj= { date: this.state.date,
+                   name: this.state.name,
+                   category: this.state.category,
+                   place_of_purchase: this.state.placeOfPurchase,
+                   out_of_pocket: this.state.outOfPocket,
+                   actual_paid: this.state.actualPaid,
+                   payment_method: this.state.paymentMethod,
+                   user_id: localStorage.user_id
+                 }
+
+     this.setState({
+        purchases: [...this.state.purchases, purObj],
+        date:"",
+        name:"",
+        category:"",
+        placeOfPurchase:"",
+        outOfPocket:"",
+        actualPaid:"",
+        paymentMethod:"",
+        selected: ""
+      })
+
+       fetch(`http://localhost:3000/purchases`, {
+         method: 'POST',
+         headers: {
+           "Content-Type": "application/json",
+         },
+         body: JSON.stringify({
+           date: this.state.date,
+           name: this.state.name,
+           category: this.state.category,
+           place_of_purchase: this.state.placeOfPurchase,
+           out_of_pocket: this.state.outOfPocket,
+           actual_paid: this.state.actualPaid,
+           payment_method: this.state.paymentMethod,
+           user_id: localStorage.user_id
+         })
+       })
+   }
+
+   editHandler = (e) => {
+     let clicked = this.state.purchases.find((purchase) => {
+       return parseInt(e.currentTarget.id) === purchase.id
+     })
+     let notClicked = this.state.purchases.filter((purchase) => {
+       return parseInt(e.currentTarget.id) !== purchase.id
+     })
+       this.setState({
+         date:clicked.date,
+         name:clicked.name,
+         category:clicked.category,
+         placeOfPurchase:clicked.place_of_purchase,
+         outOfPocket:clicked.out_of_pocket,
+         actualPaid:clicked.actual_paid,
+         paymentMethod:clicked.payment_method,
+         selected:clicked.selected,
+         purchases: notClicked
+       })
+       fetch(`http://localhost:3000/${localStorage.user_id}/purchases/${e.currentTarget.id}`, {
+         method: 'DELETE'
+       }).then(() => {
+         console.log('removed');
+       }).catch(err => {
+         console.error(err)
+       })
+
+   }
+
+   deleteHandler = (e) => {
+     let notClicked = this.state.purchases.filter((purchase) => {
+       return parseInt(e.currentTarget.dataset.id) !== purchase.id
+     })
+       this.setState({
+         purchases: notClicked
+       })
+      fetch(`http://localhost:3000/${localStorage.user_id}/purchases/${e.currentTarget.dataset.id}`, {
+        method: 'DELETE'
+      }).then(() => {
+         console.log('removed');
+      }).catch(err => {
+        console.error(err)
+      });
+      alert('Purchase deleted')
+   }
 
   render() {
     return (
@@ -30,34 +164,34 @@ class Profile extends React.Component {
           </Grid.Column>
           <Grid.Column width={4}>
             <UserStats
-            spent={this.props.spent}
-            purchases={this.props.purchases}
+            spent={this.state.spent}
+            purchases={this.state.purchases}
             />
           </Grid.Column>
           <Grid.Column width={8}>
             <SpendStats
-            spent={this.props.spent}
-            purchases={this.props.purchases}
+            spent={this.state.spent}
+            purchases={this.state.purchases}
             />
           </Grid.Column>
         </Grid.Row>
         <Grid.Row columns={1}>
           <PurchaseContainer
-          editHandler = {this.props.editHandler}
-          deleteHandler = {this.props.deleteHandler}
-          handleChange = {this.props.handleChange}
-          handleSubmit = {this.props.handleSubmit}
-          handlePaymentChange = {this.props.handlePaymentChange}
-          handleCategoryChange = {this.props.handleCategoryChange}
-          selected = {this.props.selected}
-          date={this.props.date}
-          name={this.props.name}
-          category={this.props.category}
-          placeOfPurchase={this.props.placeOfPurchase}
-          outOfPocket={this.props.outOfPocket}
-          actualPaid={this.props.actualPaid}
-          paymentMethod={this.props.paymentMethod}
-          purchases={this.props.purchases}
+          editHandler = {this.state.editHandler}
+          deleteHandler = {this.state.deleteHandler}
+          handleChange = {this.state.handleChange}
+          handleSubmit = {this.state.handleSubmit}
+          handlePaymentChange = {this.state.handlePaymentChange}
+          handleCategoryChange = {this.state.handleCategoryChange}
+          selected = {this.state.selected}
+          date={this.state.date}
+          name={this.state.name}
+          category={this.state.category}
+          placeOfPurchase={this.state.placeOfPurchase}
+          outOfPocket={this.state.outOfPocket}
+          actualPaid={this.state.actualPaid}
+          paymentMethod={this.state.paymentMethod}
+          purchases={this.state.purchases}
           />
         </Grid.Row>
       </Grid>
