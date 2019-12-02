9c05f12e42be204b559f574a190f238a0e47e510
Kevin Malazarte <Kevin.Malazarte@gmail.com>
2019-12-02 14:01:34 -0500

    created some options for dropdown

--- src/Components/viewSelector.js
+++ src/Components/viewSelector.js
@@ -1,10 +1,34 @@
 import React, {Fragment} from 'react'
 import { Dropdown } from 'semantic-ui-react'
 
+const viewOptions = [
+  {
+    key: 'Current Month',
+    text: 'Current Month',
+    value: 'Current Month'
+  },
+  {
+    key: 'Last Month',
+    text: 'Last Month',
+    value: 'Last Month'
+  },
+  {
+    key: '2 Months Ago',
+    text: '2 Months Ago',
+    value: '2 Months Ago'
+  },
+]
+
+
 const viewSelector = () => {
   return (
     <Fragment>
-      Hello World
+      <Dropdown
+       placeholder='Please Select Purchase View'
+       fluid
+       selection
+       options={viewOptions}
+      />
     </Fragment>
   )
 }
